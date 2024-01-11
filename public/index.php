5577dcfd21e900e983d6cf88eaa3e6a5000f612c
Dmitry <kdi17012021@gmail.com>
2024-01-11 20:20:31 +0300

    03 - framework structure

--- public/index.php
+++ public/index.php
@@ -1,1 +1,9 @@
-<h1>Привет!</h1>
+<?php
+
+if(PHP_MAJOR_VERSION < 8){
+    die('небходима восьмая пхп или новее');
+};
+
+require_once dirname(__DIR__) . '/config/init.php';
+
+echo 'privet';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wfm/wfm',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b232201906d3128007712eb04af6e4de2c023b5f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wfm/wfm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b232201906d3128007712eb04af6e4de2c023b5f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
